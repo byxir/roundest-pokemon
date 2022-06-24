cc7c92536fd8eb35a168da718ae563b79ec73d55
Egor Solodovnikov <egor.solo1@yandex.ru>
2022-06-24 18:30:13 +0200

    added trpc

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,11 @@ import Image from 'next/image'
 const Home: NextPage = () => {
   return (
     <div className='h-screen w-screen text-white grid justify-items-center items-center'>
-      <h1>hi</h1>
+      <div className='border-primary border-2 rounded-lg grid grid-cols-3 p-8'>
+        <div className='bg-secondary w-16 h-16 rounded-md'></div>
+        <h1 className='grid justify-items-center items-center'>VS</h1>
+        <div className='bg-secondary w-16 h-16 rounded-md'></div>
+      </div>
     </div>
   )
 }
