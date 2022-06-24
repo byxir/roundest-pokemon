ec3076e0b82c0110d96fadef74e7e9dcb947bb23
Egor Solodovnikov <egor.solo1@yandex.ru>
2022-06-24 17:17:53 +0200

    added initial styles config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: '#201E20',
+        primary: '#E0A96D',
+        secondary: '#DDC3A5',
+      },
+    },
+  },
+  plugins: [],
+}
