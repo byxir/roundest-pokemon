18ba86b4e68a6bd6fabd9c795129b43bbf26a9e3
Egor Solodovnikov <egor.solo1@yandex.ru>
2022-06-29 23:20:34 +0200

    fixed image srcs (probably)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,7 +40,7 @@ const Home: NextPage = () => {
           secondPokemon.data && (
             <>
               <div className='w-64 text-xl text-center rounded-md'>
-                <img src={`${firstPokemon.data.spriteUrl}`} className='w-64' />
+                <img src={firstPokemon.data.spriteUrl} className='w-64' />
                 <div className='capitalize'>{firstPokemon.data?.name}</div>
                 <div className='p-2'></div>
                 <button
@@ -52,7 +52,7 @@ const Home: NextPage = () => {
               </div>
               <h1 className='grid items-center justify-items-center'>VS</h1>
               <div className='w-64 text-xl text-center rounded-md '>
-                <img className='w-64' src={`${secondPokemon.data.spriteUrl}`} />
+                <img className='w-64' src={secondPokemon.data.spriteUrl} />
                 <div className='capitalize'>{secondPokemon.data?.name}</div>
                 <div className='p-2'></div>
                 <button
